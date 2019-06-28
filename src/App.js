60d01357c3083b77962b2429aeea954ab3cc2ee0
sparikh3 <sparikh3@binghamton.edu>
2019-06-28 01:05:04 -0400

    CHanged: App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          changed!!!!!!xxxxxxxxxxxxxxYYYYYYYY
+          React + Docker + AWS is Love
         </p>
         <a
           className="App-link"
